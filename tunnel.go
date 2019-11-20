ec851dfbbce2e41d11b0b9076445cfc20d31ea39
sokolas <sunrise.beast@gmail.com>
2019-11-20 11:30:19 +0300

    networking 101

--- tunnel.go
+++ tunnel.go
@@ -5,9 +5,20 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"net"
 	// "net/url"
 	// "github.com/gorilla/websocket"
 )
+
+func handleConnection(from net.Conn) {
+	defer from.Close()
+	fmt.Println("New connection accepted", from.RemoteAddr())
+	//to, err := net.Dial("tcp", *remoteAddr) - TODO dial websocket
+	// if err != nil {
+		// handle error
+	// }
+	// run copy
+}
 
 func main() {
 	interrupt := make(chan os.Signal, 1)
@@ -15,13 +26,12 @@ func main() {
 
 	localPort := flag.Int("lport", 1488, "local port to listen")
 	remoteAddr := flag.String("remote", "localhost:1488", "remote address:port to connect to")
-
 	wsAddr := flag.String("ws", "ws://localhost:1080/ws", "websocket URL")
 
 	flag.Parse()
-	fmt.Println(*localPort)
-	fmt.Println(*remoteAddr)
-	fmt.Println(*wsAddr)
+	fmt.Println("Local port", *localPort)
+	fmt.Println("Remote address", *remoteAddr)
+	fmt.Println("WebSocket URL", *wsAddr)
 
 
 	signal.Notify(interrupt, syscall.SIGINT)
@@ -32,7 +42,31 @@ func main() {
 		done <- true
 	}()
 
-	fmt.Println("Waiting...")
+
+	ln, err := net.Listen("tcp", "127.0.0.1:" + fmt.Sprintf("%d", *localPort))
+	defer ln.Close()
+	if err != nil {
+		fmt.Println("Listening error", err)
+		return
+	}
+	
+	// accepted := make(chan *Conn, 1)
+	go func() {
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				// handle error
+				fmt.Println("Listener:", err)
+				return
+			}
+
+			go handleConnection(conn)
+		}
+	}()
+
+
+
+	fmt.Println("Ready")
 	<- done
 	fmt.Println("Done")
 }
