0485092d604b9d762542521991abb5fe410ecbad
sokolas <sunrise.beast@gmail.com>
2019-11-22 03:14:03 +0300

    tunnel implementation

--- tunnel.go
+++ tunnel.go
@@ -1,33 +1,82 @@
 package main
 import (
 	"flag"
+	"errors"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 	"net"
+	"net/http"
 	"net/url"
-	// "github.com/gorilla/websocket"
+	"github.com/gorilla/websocket"
+	"github.com/google/uuid"
 )
 
-func handleConnection(from net.Conn, u string) {
+func handleConnection(tcp net.Conn, u string) {
+	defer tcp.Close()
+	fmt.Println("New connection accepted", tcp.RemoteAddr())
 
-	defer from.Close()
-	fmt.Println("New connection accepted", from.RemoteAddr())
-	c, _, err := websocket.DefaultDialer.Dial(u, nil)
-	if err != nil {
-		log.Fatal("dial:", err)
+	header := make(http.Header)
+	header["x-wstclient"] = []string{uuid.New().String()}
+	header["Sec-Websocket-Protocol"] = []string{"tunnel-protocol"}
+
+	ws, _, error := websocket.DefaultDialer.Dial(u, header)
+	if error != nil {
+		log.Println("dial:", error)
+		return
 	}
-	defer c.Close()
+	defer ws.Close()
 
-	done := make(chan error)
+	done := make(chan interface{})
 	defer close(done)
 	// make 2 goroutines, one for tcp reading, one for ws reading
 	// both write to done on exit/error
+	go func() {
+		for {
+			_, bytes, err := ws.ReadMessage()
+			if err != nil {
+				log.Println("ws read error:", err)
+				done <- err
+				return
+			}
+			n, err := tcp.Write(bytes)
+			if err != nil {
+				log.Println("tcp write error:", err)
+				done <- err
+				return
+			}
+			if n != len(bytes) {
+				err = errors.New("Failed to write all data")
+				log.Println("tcp rwite error:", err)
+				done <- err
+				return
+			}
+		}
+	}()
 
+	go func() {
+		bytes := make([]byte, 2048)
+		for {
+			n, err := tcp.Read(bytes)
+			if err != nil {
+				log.Println("tcp read error:", err)
+				done <- err
+				return
+			}
+			err = ws.WriteMessage(websocket.BinaryMessage, bytes[:n])
+			if err != nil {
+				log.Println("ws write error:", err)
+				done <- err
+				return
+			}
+		}
+	}()
 	err1, err2 := <- done, <- done
-
+	log.Println("Done with", tcp.RemoteAddr())
+	log.Println(err1, err2)
+	// run deferred closes
 }
 
 func main() {
@@ -36,7 +85,7 @@ func main() {
 
 	localPort := flag.Int("lport", 1488, "local port to listen")
 	remoteAddr := flag.String("remote", "localhost:1488", "remote address:port to connect to")
-	wsAddr := flag.String("ws", "ws://localhost:1080/ws", "websocket URL")
+	wsAddr := flag.String("ws", "ws://localhost:8000/ws", "websocket URL")
 
 	flag.Parse()
 	log.Println("Local port", *localPort)
@@ -74,7 +123,7 @@ func main() {
 				return
 			}
 
-			go handleConnection(conn, *wsAddr + "/?dst=" + remoteAddr)
+			go handleConnection(conn, *wsAddr + "/?dst=" + *remoteAddr)
 		}
 	}()
 
